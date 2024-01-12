55e983a7d65025796c70591b3cb1bb2f450c13e4
Gleb Veklenko <glebveklenko59@gmail.com>
2024-01-12 15:43:24 +0200

    main

---
+++ src/js/2-form.js
@@ -0,0 +1,28 @@
+  const form = document.querySelector('.feedback-form');
+
+  const localData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+  form.email.value = localData.email || '';
+  form.message.value = localData.message || '';
+
+  form.addEventListener('input', (event) => {
+      const currentData = {
+        email: form.email.value.trim(),
+        message: form.message.value.trim()
+      };
+      localStorage.setItem("feedback-form-state", JSON.stringify(currentData));
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = form.email.value.trim();
+    const message = form.message.value.trim();
+
+    if (email !== '' && message !== '') {
+      console.log({ email, message });
+    localStorage.clear();
+      form.reset();
+    } else {
+      alert("Please fill fields!");
+    }
+  });
